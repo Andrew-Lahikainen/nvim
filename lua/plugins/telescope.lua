684c4c6079af87bd616a9fe6047fa15fe211ed2c
Andrew Lahikainen <lahikainen.andrew@gmail.com>
2025-03-18 19:54:08 +1300

    allow telescope to search hidden files (dotfiles)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,15 +4,37 @@ return {
         tag = "0.1.8",
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = {
-            
+            find_command = { 'rg', '--files', '--hidden', '-g', '!.git' }
         },
         keys = function ()
             local builtin = require("telescope.builtin")
             return {
-                { "<leader>ff", builtin.find_files, desc = "Telescope find files", mode = "n" },
-                { "<leader>fg", builtin.live_grep, desc = "Telescope live grep", mode = "n" },
-                { "<leader>fb", builtin.buffers, desc = "Telescope buffers", mode = "n" },
-                { "<leader>fh", builtin.help_tags, desc = "Telescope help tags", mode = "n" },
+                {
+                    "<leader>ff",
+                    function()
+                        builtin.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git'} })
+                    end,
+                    desc = "Telescope find files",
+                    mode = "n"
+                },
+                {
+                    "<leader>fg",
+                    builtin.live_grep,
+                    desc = "Telescope live grep",
+                    mode = "n"
+                },
+                { 
+                    "<leader>fb",
+                    builtin.buffers,
+                    desc = "Telescope buffers",
+                    mode = "n"
+                },
+                {
+                    "<leader>fh",
+                    builtin.help_tags,
+                    desc = "Telescope help tags",
+                    mode = "n"
+                },
             }
         end,
     }
