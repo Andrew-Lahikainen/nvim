29137c268b7ad048f29bde6bcb5f68921a239c3e
Andrew Lahikainen <lahikainen.andrew@gmail.com>
2025-03-19 22:13:13 +1300

    add basic NeoTree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,4 +6,44 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
+	opts = {
+		close_if_last_window = false,
+		enable_git_status = true,
+		filesystem = {
+			filtered_items = {
+				visible = true,
+			},
+		},
+		never_show = {
+			".DS_Store",
+			"thumbs.db",
+		},
+		git_status = {
+			window = {
+				position = "float",
+				mappings = {
+					["A"] = "git_add_all",
+					["gu"] = "git_unstage_file",
+					["ga"] = "git_add_file",
+					["gr"] = "git_revert_file",
+					["gc"] = "git_commit",
+					["gp"] = "git_push",
+				},
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>nt",
+			"<cmd>Neotree toggle<cr>",
+			desc = "Open NeoTree",
+			mode = "n",
+		},
+		{
+			"<leader>gs",
+			"<cmd>Neotree git_status<cr>",
+			desc = "Neotree git status",
+			mode = "n",
+		},
+	},
 }
