29973d809c7ac8e4c3a8c39a685330434553a342
Andrew Lahikainen <lahikainen.andrew@gmail.com>
2025-04-04 22:19:26 +1300

    add tab completion for commands and add descriptions to commands

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,7 +32,20 @@ return {
 					["<C-e>"] = cmp.mapping.abort(),
 					["<C-u>"] = cmp.mapping.scroll_docs(-4),
 					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					-- accept currently selected item:
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						-- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+						if cmp.visible() then
+							local entry = cmp.get_selected_entry()
+							if not entry then
+								cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+							end
+							cmp.confirm()
+						else
+							fallback()
+						end
+					end, { "i", "s", "c" }),
 				}),
 				snippet = {
 					expand = function(args)
@@ -41,28 +54,31 @@ return {
 				},
 			})
 
+			-- https://github.com/hrsh7th/nvim-cmp/issues/1511
+			vim.keymap.set("c", "<Tab>", "<C-z>", { silent = false })
+
 			local lsp_cmds = vim.api.nvim_create_augroup("lsp_cmds", { clear = true })
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = lsp_cmds,
 				desc = "LSP actions",
 				callback = function()
-					local bufmap = function(mode, lhs, rhs)
-						vim.keymap.set(mode, lhs, rhs, { buffer = true })
+					local bufmap = function(mode, lhs, rhs, desc)
+						vim.keymap.set(mode, lhs, rhs, { buffer = true, desc = desc })
 					end
 
-					bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
-					bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
-					bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
-					bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
-					bufmap("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
-					bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")
-					bufmap("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
-					bufmap("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>")
-					bufmap("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>")
-					bufmap("n", "gl", "<cmd>lua vim.lsp.buf.open_float()<cr>")
-					bufmap("n", "[d", "<cmd>lua vim.lsp.buf.goto_prev()<cr>")
-					bufmap("n", "]d", "<cmd>lua vim.lsp.buf.goto_next()<cr>")
+					bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", "lsp: hover")
+					bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", "lsp: go to definition")
+					bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", "lsp: go to declaration")
+					bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", "lsp: go to implementation")
+					bufmap("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<CR>", "lsp: go to type definition")
+					bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", "lsp: go to references")
+					bufmap("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", "lsp: signature help")
+					bufmap("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>", "lsp: rename")
+					bufmap("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<CR>", "lsp: code action")
+					bufmap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", "lsp: open diagnostics float")
+					bufmap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", "lsp: go to previous diagnostic")
+					bufmap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", "lsp: go to next diagnostic")
 				end,
 			})
 
