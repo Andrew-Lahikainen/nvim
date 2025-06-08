63336c9db9898f153cd34f34fd3b67bc379505bb
Andrew Lahikainen <lahikainen.andrew@gmail.com>
2025-06-08 15:30:13 +1200

    update lsp configs

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,42 +1,32 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	---@module "conform"
-	---@type conform.setupOpts
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			typescript = { "prettierd", "prettier", stop_after_first = true },
-		},
+    "stevearc/conform.nvim",
+    cmd = { "ConformInfo" },
+    ---@module "conform"
+    ---@type conform.setupOpts
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+            typescript = { "prettierd", "prettier", stop_after_first = true },
+        },
 
-		default_format_opts = {
-			lsp_format = "fallback",
-		},
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
 
-		format_on_save = { timeout_ms = 500 },
-
-		formatters = {
-			shfmt = {
-				prepend_args = { "-i", "2" },
-			},
-		},
-	},
-	keys = function()
-		local conform = require("conform")
-		return {
-			{
-				"<leader>fm",
-				function()
-					conform.format({ async = true })
-				end,
-				desc = "Conform format file",
-				mode = "n",
-			},
-		}
-	end,
-	init = function()
-		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-	end,
+        format_on_save = { timeout_ms = 500 },
+    },
+    keys = function()
+        local conform = require("conform")
+        return {
+            {
+                "<leader>fm",
+                function()
+                    conform.format({ async = true })
+                end,
+                desc = "Conform format file",
+                mode = "n",
+            },
+        }
+    end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,16 +3,24 @@ return {
         "neovim/nvim-lspconfig",
         lazy = true,
         config = function()
-            local capabilities = require("blink.cmp").get_lsp_capabilities()
+            local blink = require("blink.cmp")
+            local capabilities = blink.get_lsp_capabilities()
 
             vim.lsp.config("*", {
                 capabilities = capabilities,
                 root_markers = { ".git" },
             })
 
+            vim.diagnostic.config({
+                float = {
+                    border = "single",
+                },
+            })
+
             vim.lsp.enable("ts_ls")
             vim.lsp.enable("lua_ls")
             vim.lsp.enable("astro")
+            vim.lsp.enable("cssls")
 
             vim.lsp.inlay_hint.enable(true, { 0 })
         end,
@@ -20,87 +28,87 @@ return {
             {
                 "K",
                 function()
-                    vim.lsp.buf.hover()
+                    vim.lsp.buf.hover({ border = "rounded" })
                 end,
-                desc = "lsp: hover"
+                desc = "lsp: hover",
             },
             {
                 "gd",
                 function()
                     vim.lsp.buf.definition()
                 end,
-                desc = "lsp: go to definition"
+                desc = "lsp: go to definition",
             },
             {
                 "gD",
                 function()
                     vim.lsp.buf.declaration()
                 end,
-                desc = "lsp: go to declaration"
+                desc = "lsp: go to declaration",
             },
             {
                 "gi",
                 function()
                     vim.lsp.buf.implementation()
                 end,
-                desc = "lsp: go to implementation"
+                desc = "lsp: go to implementation",
             },
             {
                 "go",
                 function()
                     vim.lsp.buf.type_definition()
                 end,
-                desc = "lsp: go to type definition"
+                desc = "lsp: go to type definition",
             },
             {
                 "gr",
                 function()
                     vim.lsp.buf.references()
                 end,
-                desc = "lsp: go to references"
+                desc = "lsp: go to references",
             },
             {
                 "gs",
                 function()
                     vim.lsp.buf.signature_help()
                 end,
-                desc = "lsp: signature help"
+                desc = "lsp: signature help",
             },
             {
                 "<F2>",
                 function()
                     vim.lsp.buf.rename()
                 end,
-                desc = "lsp: rename"
+                desc = "lsp: rename",
             },
             {
                 "<F4>",
                 function()
                     vim.lsp.buf.code_action()
                 end,
-                desc = "lsp: code action"
+                desc = "lsp: code action",
             },
             {
                 "gl",
                 function()
-                    vim.lsp.buf.open_float()
+                    vim.diagnostic.open_float()
                 end,
-                desc = "lsp: open diagnostics float"
+                desc = "lsp: open diagnostics float",
             },
             {
                 "[d",
                 function()
                     vim.lsp.buf.goto_prev()
                 end,
-                desc = "lsp: go to previous diagnostic"
+                desc = "lsp: go to previous diagnostic",
             },
             {
                 "]d",
                 function()
                     vim.lsp.buf.goto_next()
                 end,
-                desc = "lsp: go to next diagnostic"
-            }
+                desc = "lsp: go to next diagnostic",
+            },
         },
     },
     {
@@ -115,11 +123,11 @@ return {
                 },
                 automatic_enable = true,
             })
-        end
+        end,
     },
     {
         "mason-org/mason.nvim",
         event = "VeryLazy",
-        opts = {}
-    }
+        opts = {},
+    },
 }
